813f6511f18210ef4f1a0b5ccf5c8110759f8748
nanocloudx <nanocloudx@gmail.com>
2017-11-03 14:06:20 +0900

    fix sequelize config

--- server/database/config.js
+++ server/database/config.js
@@ -1,19 +1,13 @@
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
+    url: process.env.DATABASE_URL,
     dialect: "postgres",
     dialectOptions: {
       timezone: "+09:00"
     }
   },
   production: {
-    username: 'database_dev',
-    password: 'database_dev',
-    database: 'database_dev',
-    host: '127.0.0.1',
+    url: process.env.DATABASE_URL,
     dialect: "postgres",
     dialectOptions: {
       ssl: {
