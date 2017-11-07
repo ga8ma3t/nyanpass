d5d95859d50a51eba5d54a84575a9ceac6580b6e
nanocloudx <nanocloudx@gmail.com>
2017-11-07 20:18:23 +0900

    init client

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,50 @@
 const path = require('path');
 
 module.exports = {
-  entry: './client/app.js',
+  entry: './client/main.js',
   output: {
     filename: 'bundle.js',
     path: path.join(__dirname, 'dist/public/')
   },
   module: {
     rules: [
-      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+            'scss': [
+              'vue-style-loader',
+              'css-loader',
+              'sass-loader'
+            ],
+              'sass': [
+              'vue-style-loader',
+              'css-loader',
+              'sass-loader?indentedSyntax'
+            ]
+          }
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          'sass-loader'
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
     ]
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    },
+    extensions: ['*', '.js', '.vue', '.json']
   }
 }
