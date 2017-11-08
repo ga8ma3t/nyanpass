acc1dc5e91627a58de9ce17f9b21c9d383fad62a
nanocloudx <nanocloudx@gmail.com>
2017-11-08 20:42:41 +0900

    show event list

--- client/components/Index.vue
+++ client/components/Index.vue
@@ -1,16 +1,34 @@
 <template>
   <div class="index">
     <h2>イベント一覧</h2>
-    <router-link to="/catalogue/c93">コミックマーケット93</router-link>
+    <ul>
+      <li v-for="event in eventList">
+        <router-link :to="`/catalogue/${event.alternateId}`">{{event.name}}</router-link>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+  import request from 'axios'
   export default {
     name: 'index',
     data() {
       return {
-        //
+        eventList: null
+      }
+    },
+    created() {
+      this.fetchEventList()
+    },
+    watch: {
+      '$route': 'fetchEventList'
+    },
+    methods: {
+      fetchEventList() {
+        request.get(`/api/events`).then(result => {
+          this.eventList = result.data
+        })
       }
     }
   }
