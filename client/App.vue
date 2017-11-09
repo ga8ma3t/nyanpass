63a9057283859c7d9821834078cdbbf2a6a6bc40
nanocloudx <nanocloudx@gmail.com>
2017-11-09 21:59:30 +0900

    add layout components

--- client/App.vue
+++ client/App.vue
@@ -1,25 +1,35 @@
 <template>
   <div id="app">
-    <header>
-      <router-link to="/"><h1>にゃんぱす！v2</h1></router-link>
-    </header>
-    <router-view></router-view>
-    <footer>
-      <ul>
-        <li>
-          <router-link to="/about"><p>このサイトについて</p></router-link>
-        </li>
-        <li v-if="session">
-          <a href="/auth/logout"><p>ログアウト</p></a>
-        </li>
-      </ul>
-    </footer>
+    <el-container>
+      <el-header>
+        <router-link to="/"><h1>にゃんぱす！v2</h1></router-link>
+      </el-header>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+      <el-footer>
+        <ul>
+          <li>
+            <router-link to="/about"><p>このサイトについて</p></router-link>
+          </li>
+          <li v-if="session">
+            <a href="/auth/logout"><p>ログアウト</p></a>
+          </li>
+        </ul>
+      </el-footer>
+    </el-container>
   </div>
 </template>
 
 <script>
   import request from 'axios'
+  import ElMain from '../node_modules/element-ui/packages/main/src/main.vue'
+  import ElContainer from '../node_modules/element-ui/packages/container/src/main.vue'
   export default {
+    components: {
+      ElContainer,
+      ElMain
+    },
     name: 'app',
     data() {
       return {
@@ -44,7 +54,7 @@
 
 <style lang="scss">
   #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    font-family: "Helvetica Neue", Helvetica, "Hiragino Sans GB", Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   }
--- client/main.js
+++ client/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import { Button } from 'element-ui'
+import {
+  Container,
+  Header,
+  Main,
+  Footer,
+  Row,
+  Col,
+  Button
+} from 'element-ui'
 
+// Layouts
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Main)
+Vue.use(Footer)
+Vue.use(Row)
+Vue.use(Col)
+
+// Forms
 Vue.use(Button)
 
 export default new Vue({
