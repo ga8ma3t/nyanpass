f728ae89f6b346cb1f5a0faf4df19e0576efb317
kokuyouwind <kokuyouwind@gmail.com>
2017-11-25 19:06:07 +0900

    use dates column on eventsApi

--- server/apis/event.js
+++ server/apis/event.js
@@ -1,27 +1,13 @@
-// import {fetchEventByAlternateId, fetchEvents} from '../models/event'
+import {fetchEventByAlternateId, fetchEvents} from '../models/event'
 
 export function fetchEventList(req, res) {
-  res.json([{
-    alternateId: 'c93',
-    dates: ['2017-12-29', '2017-12-30', '2017-12-31'],
-    id: 'a2894003-50e1-4cbb-898b-db31aee96996',
-    name: 'コミックマーケット93',
-    place: '東京ビッグサイト'
-  }])
-  // fetchEvents().then(result => {
-  //   res.json(result)
-  // })
+  fetchEvents().then(result => {
+    res.json(result)
+  })
 }
 
 export function fetchEvent(req, res) {
-  res.json({
-    alternateId: 'c93',
-    dates: ['2017-12-29', '2017-12-30', '2017-12-31'],
-    id: 'a2894003-50e1-4cbb-898b-db31aee96996',
-    name: 'コミックマーケット93',
-    place: '東京ビッグサイト'
+  fetchEventByAlternateId(req.params.eventId).then(result => {
+    res.json(result)
   })
-  // fetchEventByAlternateId(req.params.eventId).then(result => {
-  //   res.json(result)
-  // })
 }
--- server/models/event.js
+++ server/models/event.js
@@ -2,10 +2,15 @@ import {Event} from '../database/models/index'
 
 export function fetchEvents() {
   return Event.findAll({
-    attributes: ['alternateId', 'name', 'date', 'place']
-  })
+    attributes: ['alternateId', 'name', 'dates', 'place']
+  }).then((events) => events.map(datesToArray))
 }
 
 export function fetchEventByAlternateId(id) {
   return Event.findOne({where: { alternateId: id }})
+    .then((event) => datesToArray(event.get({plain: true})))
+}
+
+function datesToArray(event) {
+  return Object.assign(event, { dates: event.dates.split(',') })
 }
