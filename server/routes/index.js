ab70abb6fcd1a32c25d4f239cdf73c498b2dccde
kokuyouwind <kokuyouwind@gmail.com>
2017-11-25 14:12:31 +0900

    不要になったdangerzoneを削除

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,14 +1,12 @@
 import express from 'express'
 import {fetchEventList, fetchEvent} from '../apis/event'
 import {fetchCatalogue} from '../apis/catalogue'
-import {extractC93LayoutReport} from '../apis/dangerzone'
 
 const router = express.Router()
 
 router.get('/api/events', fetchEventList)
 router.get('/api/events/:eventId', fetchEvent)
 router.get('/api/catalogues/:eventId', fetchCatalogue)
-router.get('/api/dangerzone/extract-c93', extractC93LayoutReport)// TODO デプロイ時には消す!!!
 router.get(['/', '/catalogues/:eventId', '/about'], (req, res) => {
   res.render('index', {
     user: req.user,
