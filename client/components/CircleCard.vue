2efd2af1bf133744fd642244c621300608efaa8c
nanocloudx <nanocloudx@gmail.com>
2017-11-25 19:20:35 +0900

    append bookmark icon

--- client/components/CircleCard.vue
+++ client/components/CircleCard.vue
@@ -5,6 +5,7 @@
       <p class="nothing" v-if="circleList.length === 0">みつかりませんでした</p>
       <div class="circle-card-container">
         <div class="circle-card" v-for="circle in circleList">
+          <p class="bookmark"></p>
           <img :src="circle.imageUrl" class="circle-card-image" onerror="this.src='/images/noimage.jpg'">
           <div class="circle-card-info">
             <p class="circle-card-info-space">{{circle.space.district}} {{circle.space.block}}-{{circle.space.space}}</p>
@@ -33,10 +34,13 @@
   h4 {
     text-align: center;
     font-size: 20px;
-    margin: 10px;
+    margin: 10px 2%;
     padding: 10px 0;
     background-color: #0084b4;
     color: #ffffff;
+    @media screen and (min-width: 635px) {
+      margin: 10px;
+    }
   }
   .nothing {
     text-align: center;
@@ -58,6 +62,7 @@
         padding: 0 10px 20px;
       }
       &:hover {
+        cursor: pointer;
         .circle-card-info {
           box-shadow: 0 1px 1px rgba(0,0,0,0.1);
         }
@@ -79,6 +84,24 @@
       }
       .circle-card-info-account-name, .circle-card-info-account-twitter {
         font-size: 12px;
+      }
+      .bookmark {
+        display: block;
+        position: absolute;
+        border-right: 50px solid #ffc423;
+        border-bottom: 50px solid transparent;
+        right: 4%;
+        @media screen and (min-width: 635px) {
+          right: 10px;
+        }
+      }
+      .bookmark:after {
+        position: absolute;
+        top: 2px;
+        right: -48px;
+        font-size: 24px;
+        content: "★";
+        color: #ffffff;
       }
     }
   }
